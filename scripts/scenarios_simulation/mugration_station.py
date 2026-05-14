b76dacc7762a21a900a1a6773a740c0f364e2dda
Andrew Warren <aswarren@gmail.com>
2026-05-14 13:21:43 -0400

    mugration station dependencies

--- scripts/scenarios_simulation/mugration_station.py
+++ scripts/scenarios_simulation/mugration_station.py
@@ -1,6 +1,8 @@
 import json
 import networkx as nx
 from collections import defaultdict, Counter
+import pandas as pd
+
 
 def build_directed_graph(df: pd.DataFrame, pid_col="sim_pid", contact_col="contact_pid") -> nx.DiGraph:
     """
